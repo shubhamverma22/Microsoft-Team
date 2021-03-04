b226325cfd27b00fb4eaad8d77945b6a08adbd49
Shubham Verma <lpushubham98@gmail.com>
2021-03-04 13:47:38 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChatEngine, ChatFeed } from "react-chat-engine";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ChatEngine
+			height="100vh"
+			projectID="6c612725-771a-4f57-ac80-881a71ded12d"
+			userName="iamdevshubham"
+			userSecret="Verma123@"
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		/>
+	);
 }
 
 export default App;
